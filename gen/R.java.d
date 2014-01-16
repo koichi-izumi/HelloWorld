d9e64bab9ee4eeeb67577880aa79a1f54085d26e
koichi-izumi <koichi_izumi@nifty.com>
2014-01-16 13:37:57 +0900

    create android project and buld by ant

---
+++ gen/R.java.d
@@ -0,0 +1,4 @@
+C:\cygwin\home\kizumi\work\android\HelloWorld\gen\com\android\example\R.java \
+ : C:\cygwin\home\kizumi\work\android\HelloWorld\res\layout\main.xml \
+C:\cygwin\home\kizumi\work\android\HelloWorld\res\values\strings.xml \
+C:\cygwin\home\kizumi\work\android\HelloWorld\bin\AndroidManifest.xml \
